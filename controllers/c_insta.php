9fe23e42b0a30ab17c4a78250d9565222b574878
lucianoBrd <lucien.burdet@gmail.com>
2019-02-17 14:48:46 +0100

    api insta

--- controllers/c_insta.php
+++ controllers/c_insta.php
@@ -1,4 +1,16 @@
 <?php
-$myhtml = file_get_contents('https://www.instagram.com/ngyemilie/?hl=fr');
+$url = "https://www.instagram.com/ngyemilie/?hl=fr";
+$curl = curl_init();
 
-echo $myhtml;
+$opts = [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_USERAGENT => 'Mozilla/5.0 (Linux; U; Android 2.1-update1; fr-fr; GTI9000 Build/ECLAIR) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17',
+];
+
+curl_setopt_array($curl, $opts);
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+echo $response;
