92b738887f8691494199caedccc42fe29096c9a0
lucianoBrd <lucien.burdet@gmail.com>
2019-03-05 14:59:40 +0100

    new font tarif info

--- views/v_tarifs.php
+++ views/v_tarifs.php
@@ -36,7 +36,7 @@
 
     <div class="row">
 
-      <div class="col-sm-6 col-sm-offset-3">
+      <div class="col-sm-6 col-sm-offset-3" id="tarifsinfo">
         <p>Je suis là pour vous mettre à l’aise, pour vous accompagner tout au long du shooting dans la bonne humeur !</p>
         <p>Je suis également à votre disposition après le shooting si vous avez besoin de conseils, ou avant le shooting pour toutes questions !</p>
         <ul>
@@ -63,78 +63,6 @@
     <div class="row">
       <div class="col-sm-6 col-sm-offset-3">
         <h2 class="module-title font-alt">Etudiant</h2>
-      </div>
-    </div>
-    <!-- /MODULE TITLE -->
-
-    <div class="row multi-columns-row">
-
-      <!-- PRICE-TABLE -->
-      <div class="col-sm-6 col-md-4 col-lg-4">
-        <div class="price-table font-alt">
-          <h4>Light</h4>
-          <div class="borderline"></div>
-          <p class="price"><span>€</span>25</p>
-          <ul class="price-details">
-            <li>30 minutes de shooting</li>
-            <li>3 photos retouchées</li>
-            <li>Et envoyées en HD</li>
-          </ul>
-          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
-        </div>
-      </div>
-      <!-- /PRICE-TABLE -->
-
-      <!-- PRICE-TABLE -->
-      <div class="col-sm-6 col-md-4 col-lg-4">
-        <div class="price-table best font-alt">
-          <h4>Avancé</h4>
-          <div class="borderline"></div>
-          <p class="price"><span>€</span>55</p>
-          <ul class="price-details">
-            <li>1 heure de shooting</li>
-            <li>10 photos retouchées</li>
-            <li>Et envoyées en HD</li>
-          </ul>
-          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
-        </div>
-      </div>
-      <!-- /PRICE-TABLE -->
-
-      <!-- PRICE-TABLE -->
-      <div class="col-sm-6 col-md-4 col-lg-4">
-        <div class="price-table font-alt">
-          <h4>Basic</h4>
-          <div class="borderline"></div>
-          <p class="price"><span>€</span>45</p>
-          <ul class="price-details">
-            <li>45 minutes de shooting</li>
-            <li>7 photos retouchées</li>
-            <li>Et envoyées en HD</li>
-          </ul>
-          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
-        </div>
-      </div>
-      <!-- /PRICE-TABLE -->
-
-    </div>
-
-  </div>
-
-</section>
-<!-- /PRICING 3 COLUMNS -->
-
-<hr class="divider"><!-- DIVIDER -->
-
-<!-- PRICING 3 COLUMNS -->
-<section class="module">
-
-  <div class="container">
-
-    <!-- MODULE TITLE -->
-    <div class="row">
-      <div class="col-sm-6 col-sm-offset-3">
-        <h2 class="module-title font-alt">Normal</h2>
       </div>
     </div>
     <!-- /MODULE TITLE -->
@@ -179,6 +107,78 @@
           <h4>Basic</h4>
           <div class="borderline"></div>
           <p class="price"><span>€</span>50</p>
+          <ul class="price-details">
+            <li>45 minutes de shooting</li>
+            <li>7 photos retouchées</li>
+            <li>Et envoyées en HD</li>
+          </ul>
+          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
+        </div>
+      </div>
+      <!-- /PRICE-TABLE -->
+
+    </div>
+
+  </div>
+
+</section>
+<!-- /PRICING 3 COLUMNS -->
+
+<hr class="divider"><!-- DIVIDER -->
+
+<!-- PRICING 3 COLUMNS -->
+<section class="module">
+
+  <div class="container">
+
+    <!-- MODULE TITLE -->
+    <div class="row">
+      <div class="col-sm-6 col-sm-offset-3">
+        <h2 class="module-title font-alt">Normal</h2>
+      </div>
+    </div>
+    <!-- /MODULE TITLE -->
+
+    <div class="row multi-columns-row">
+
+      <!-- PRICE-TABLE -->
+      <div class="col-sm-6 col-md-4 col-lg-4">
+        <div class="price-table font-alt">
+          <h4>Light</h4>
+          <div class="borderline"></div>
+          <p class="price"><span>€</span>35</p>
+          <ul class="price-details">
+            <li>30 minutes de shooting</li>
+            <li>3 photos retouchées</li>
+            <li>Et envoyées en HD</li>
+          </ul>
+          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
+        </div>
+      </div>
+      <!-- /PRICE-TABLE -->
+
+      <!-- PRICE-TABLE -->
+      <div class="col-sm-6 col-md-4 col-lg-4">
+        <div class="price-table best font-alt">
+          <h4>Avancé</h4>
+          <div class="borderline"></div>
+          <p class="price"><span>€</span>65</p>
+          <ul class="price-details">
+            <li>1 heure de shooting</li>
+            <li>10 photos retouchées</li>
+            <li>Et envoyées en HD</li>
+          </ul>
+          <a href="?page=contact" class="btn btn-d btn-round">Me contacter</a>
+        </div>
+      </div>
+      <!-- /PRICE-TABLE -->
+
+      <!-- PRICE-TABLE -->
+      <div class="col-sm-6 col-md-4 col-lg-4">
+        <div class="price-table font-alt">
+          <h4>Basic</h4>
+          <div class="borderline"></div>
+          <p class="price"><span>€</span>55</p>
           <ul class="price-details">
             <li>45 minutes de shooting</li>
             <li>7 photos retouchées</li>
